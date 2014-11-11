f1233d41f035f39e51f1e95a7b7faf3940845506
hminh1208 <hminh1208@gmail.com>
2014-11-12 00:23:38 +0700

    add to repo

--- What's The Weather/ViewController.swift
+++ What's The Weather/ViewController.swift
@@ -7,12 +7,65 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var city: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func findWeather(sender: AnyObject) {
+        self.view.endEditing(true)
+        
+        
+        var urlString = "http://www.weather-forecast.com/locations/"+city.text.stringByReplacingOccurrencesOfString(" ", withString: "")+"/forecasts/latest"
+        
+        var url = NSURL(string: urlString)
+        
+       
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding)
+            
+           println(response)
+            
+            let tempurlContent: String = urlContent as String
+            
+            if (urlContent! as NSString).containsString("<span class=\"phrase\">") {
+                
+                var contentArray = (urlContent! as NSString).componentsSeparatedByString("<span class=\"phrase\">")
+                
+                var newContentArray = contentArray[1].componentsSeparatedByString("</span>")
+                
+                var weatherForcast = newContentArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    
+                    self.result.text = weatherForcast
+                    
+                }
+                
+            } else {
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    
+                    self.result.text = "Could not find that city - please try again"
+                }
+            }
+        }
+        
+        task.resume()
+    }
+        
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
